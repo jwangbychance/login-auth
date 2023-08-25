a68329a406e53702d0a7024c982b416de96da1f8
jwangbychance <jackson.wang1998@outlook.com>
2023-08-25 20:11:47 +1000

    use es6 import statements and remove console log

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
-require("dotenv").config();
-const express = require("express");
-const path = require("path");
-const mongoose = require("mongoose");
+import "dotenv/config.js";
+import express from "express";
+import path from "path";
+import mongoose from "mongoose";
+import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
@@ -18,7 +22,6 @@ app.listen(PORT, () => {
 
 mongoose.set("strictQuery", false);
 const mongoDB = process.env.MONGO_CONNECTION;
-console.log(mongoDB);
 
 main().catch((err) => console.log(err));
 async function main() {
