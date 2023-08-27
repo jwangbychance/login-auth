e87862163c2e65abafc6a206bde6c00544cbdb84
jwangbychance <jackson.wang1998@outlook.com>
2023-08-27 22:28:51 +1000

    move signup to own component and remove boilerplate svgs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,47 +1,11 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import axios from "axios";
+import Signup from "./components/Signup";
 
 function App() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  const signup = async (e) => {
-    e.preventDefault();
-    try {
-      await axios.post("/api/sign-up", { username, password }).then((res) => {
-        if (res.status === 200 && window) {
-          window.location.href = "/profile";
-          // or <Redirect to="/profile" /> if using react-router
-        }
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
   return (
-    <div className="">
-      <h1 className="">Sign Up</h1>
-      <form action="" method="POST" onSubmit={signup}>
-        <label for="username">Username</label>
-        <input
-          name="username"
-          placeholder="username"
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <label for="password">Password</label>
-        <input
-          name="password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button>Sign Up</button>
-      </form>
+    <div className="h-[100vh] flex justify-center items-center bg-gray-600">
+      <Signup />
     </div>
   );
 }
---
+++ client/src/components/Signup.jsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const Signup = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signup = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/api/sign-up", { username, password }).then((res) => {
+        if (res.status === 200 && window) {
+          window.location.href = "/profile";
+          // or <Redirect to="/profile" /> if using react-router
+        }
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="w-5/12 rounded-md bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1">
+      <div className="flex flex-col h-full w-full p-5 bg-slate-300">
+        <h1 className="self-center underline text-2xl font-bold decoration-pink-500">
+          Sign Up
+        </h1>
+        <form
+          className="flex flex-col"
+          action=""
+          method="POST"
+          onSubmit={signup}
+        >
+          <label className="font-semibold mt-5" for="username">
+            Username
+          </label>
+          <input
+            name="username"
+            className="border-2 border-pink-600 rounded-xl p-1"
+            placeholder="username"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <label className="font-semibold mt-5" for="password">
+            Password
+          </label>
+          <input
+            name="password"
+            className="border-2 border-pink-600 rounded-xl p-1"
+            placeholder="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="mt-5 border-2 border-pink-500 rounded-xl w-3/12 hover:bg-pink-300 transition-all ease-in-out duration-300 bg-white">
+            Register
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
