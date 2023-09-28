faf6217d9263fb4d958f98550115e573c735a28b
jwangbychance <jackson.wang1998@outlook.com>
2023-09-28 21:09:47 +1000

    add profile img on navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -16,15 +16,31 @@ const Navbar = ({ isLoggedIn }) => {
             <span className="text-[#8b3dff]">Login</span> Auth
           </div>
           {isLoggedIn ? (
-            <div className="flex gap-2 md:gap-8 relative">
+            <div className="flex relative items-center">
               <button
                 onClick={() => {
                   setShowProfile((prev) => !prev);
                 }}
-                className="flex gap-3 text-[#8b3dff] font-semibold px-4 md:px-8 py-1 md:py-[6px] text-md"
+                className="flex gap-3 border-[#8b3dff] font-semibold px-4 md:px-8 py-1 md:py-[6px] text-md"
               >
+                <div className="w-6 h-6 text-[#8b3dff]">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="1.5"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+                </div>
                 Profile
-                <div className="w-4 h-4 self-center text-gray-600">
+                <div className="w-4 h-4 text-gray-600 self-center">
                   <svg
                     fill="none"
                     stroke="currentColor"
