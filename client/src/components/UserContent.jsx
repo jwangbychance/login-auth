7d01fab1ee2ccde47e2c7b049e46af380a76b010
jwangbychance <jackson.wang1998@outlook.com>
2023-09-29 14:34:28 +1000

    add a side profile for logged in users

--- client/src/components/UserContent.jsx
+++ client/src/components/UserContent.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import MessageBox from "./MessageBox";
+import jokerImg from "../assets/joker_take_your_time.png";
 
 const UserContent = ({ user }) => {
   return (
-    <div className="mt-5 px-5 w-full h-full">
-      <div className="font-semibold mt-2">
-        Welcome back <span className="text-[#8b3dff]">{user}</span>! &#128526;
+    <div className="flex mt-5 px-5 w-full h-full">
+      <div className="w-full">
+        <div className="font-semibold mt-2">
+          Welcome back <span className="text-[#8b3dff]">{user}</span>! &#128526;
+        </div>
+        <div className="flex flex-col mt-5">
+          <MessageBox />
+        </div>
       </div>
-      <div className="flex flex-col mt-5">
-        <MessageBox />
+      <div className="hidden md:flex ml-auto md:border h-fit border-gray-200 rounded-md p-5 w-3/12 md:flex-col justify-center">
+        <img
+          src={jokerImg}
+          className="hidden md:block w-40 rounded-full overflow-hidden self-center"
+        />
+        <div className="hidden md:block text-sm mt-5 font-semibold">
+          Status: <span className="text-green-600">Online</span>
+        </div>
       </div>
     </div>
   );
