2f91c00130de69478c3572421b6e98a54ac5dbaf
jwangbychance <jackson.wang1998@outlook.com>
2023-11-16 15:10:39 +1100

    add unit tests for user api calls

---
+++ client/src/api/messages/messages.test.ts
@@ -0,0 +1,26 @@
+import { afterEach, describe, expect, test, vi, it } from "vitest";
+import { createMessage, viewMessages } from "../messages";
+import axios from "axios";
+
+describe("messages api call tests", async () => {
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  describe("create message", () => {
+    const axiosPostSpy = vi.spyOn(axios, "post");
+
+    it("should create a message", async () => {
+      await createMessage(
+        "username",
+        "content",
+        "Sun Nov 12 2023 22:13:20 GMT+1100 (Australian Eastern Daylight Time)"
+      );
+      expect(axiosPostSpy).toHaveBeenCalledWith("/api/create-message", {
+        username: "username",
+        content: "content",
+        date: "Sun Nov 12 2023 22:13:20 GMT+1100 (Australian Eastern Daylight Time)",
+      });
+    });
+  });
+});
---
+++ client/src/api/users/users.test.ts
@@ -0,0 +1,62 @@
+import { afterEach, describe, expect, test, vi, it } from "vitest";
+import { signupUser, loginUser, getUser, joinMemberSociety } from "../users";
+import axios from "axios";
+import "dotenv/config.js";
+
+describe("users api call tests", async () => {
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  describe("register user", () => {
+    const axiosPostSpy = vi.spyOn(axios, "post");
+
+    it("should register a user", async () => {
+      await signupUser("username", "password");
+      expect(axiosPostSpy).toHaveBeenCalledWith("/api/sign-up", {
+        username: "username",
+        password: "password",
+      });
+    });
+  });
+
+  describe("login user", () => {
+    const axiosPostSpy = vi.spyOn(axios, "post");
+
+    it("should login a user", async () => {
+      await loginUser("username", "password");
+      expect(axiosPostSpy).toHaveBeenCalledWith("/api/log-in", {
+        username: "username",
+        password: "password",
+      });
+    });
+  });
+
+  describe("get user", () => {
+    const axiosGetSpy = vi.spyOn(axios, "get");
+
+    it("should get a user", async () => {
+      await getUser();
+      expect(axiosGetSpy).toHaveBeenCalledWith("/api/user");
+    });
+
+    it("should return status and user details", async () => {
+      const { status, userDetails } = await getUser();
+      expect(status).toEqual(200);
+      expect(userDetails).toEqual({ username: "username", member: false });
+    });
+  });
+
+  describe("user joins member society", () => {
+    const axiosPutSpy = vi.spyOn(axios, "put");
+    const memberKey = process.env.MEMBER_KEY!;
+
+    it("should update user to member", async () => {
+      await joinMemberSociety("username", memberKey);
+      expect(axiosPutSpy).toHaveBeenCalledWith("/api/become-member", {
+        username: "username",
+        memberKey: memberKey,
+      });
+    });
+  });
+});
